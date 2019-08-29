0257fa3d5bfd19ec19cb1be5d9bc1d2b3dfad8fc
Dmitry Ponyatov <dponyatov@gmail.com>
2019-08-29 11:35:33 +0400

    + web
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- IoD.py
+++ IoD.py
@@ -10,6 +10,38 @@ class Frame:
         self.slot = {}
         # nested elements = vector = stack = queue
         self.nest = []
+    
+    # dump
+
+    def __repr__(self): return self.dump()
+    def dump(self,depth=0,prefix=''):
+        tree = self._pad(depth) + self.head(prefix)
+        if not depth: Frame._dumped = []
+        if self in Frame._dumped: return tree + ' _/'
+        else: Frame._dumped.append(self)
+        for i in self.slot:
+            tree += self.slot[i].dump(depth+1,prefix='%s = '%i)
+        for j in self.nest:
+            tree += i.dump(depth+1)
+        return tree
+    def head(self,prefix=''):
+        return '%s<%s:%s> @%.x' % (prefix,self.type,self._val(),id(self))
+    def _pad(self,depth):
+        return '\n' + '\t' * depth
+    def _val(self):
+        return str(self.val)
+
+    # operators
+
+    def __getitem__(self,key):
+        return self.slot[key]
+    def __setitem__(self,key,that):
+        self.slot[key] = that ; return self
+    def __lshift__(self,that):
+        if callable(that): return self << Cmd(that) # wrap py fn
+        self[that.val] = that ; return self
+    def __floordiv__(self,that):
+        self.nest.append(that) ; return self
 
 class Primitive(Frame):
     def eval(self,ctx): ctx // self
@@ -28,5 +60,39 @@ class Queue(Container): pass
 class Active(Frame): pass
     
 class VM(Active): pass
-class Cmd(Active): pass
+
+class Cmd(Active):
+    def __init__(self,F):
+        Active.__init__(self,F.__name__)
+        self.fn = F
+    def eval(self,ctx):
+        self.fn(ctx)
+
 class Seq(Active,Vector): pass
+
+class Doc(Frame): pass
+class Color(Doc): pass
+class Font(Doc): pass
+
+class IO(Frame): pass
+class Net(IO): pass
+class Ip(Net): pass
+class Port(Net): pass
+class Web(Net):
+    def __init__(self,V):
+        Net.__init__(self,V)
+        self['ip'] = Ip('127.0.0.1')
+        self['port'] = Port(8888)
+        self['back'] = Color('black')
+        self['fore'] = Color('lightgreen')
+        self['font'] = Font('monospace')
+        self['font']['size'] = Symbol('3mm')
+    def eval(self,ctx):
+        flask = __import__('flask')
+        app = flask.Flask(self.val)
+
+        @app.route('/')
+        def index():
+            return flask.render_template('index.html',ctx=ctx,web=self)
+
+        app.run(host=self['ip'].val,port=self['port'].val,debug=True)
--- metacircular.py
+++ metacircular.py
@@ -2,4 +2,15 @@
 
 from IoD import *
 
-IoD = VM('IoD') ; print IoD
+IoD = VM('IoD')
+
+IoD['W'] = IoD
+IoD['S'] = IoD
+
+def WEB(ctx): ctx['WEB'] = Web(ctx.val) ; ctx['WEB'].eval(ctx)
+IoD << WEB
+
+print(IoD)
+
+IoD['WEB'].eval(IoD)
+print(IoD)
